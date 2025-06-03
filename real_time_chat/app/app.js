6955cb768e7398cd05d0437ce9a7f6ff0c545b96
mfgs123 <mariafernandagranados12@gmail.com>
2025-06-03 17:04:54 +0100

    modifying code to work with socket.io library instead of websockets

--- real_time_chat/app/app.js
+++ real_time_chat/app/app.js
@@ -1,13 +1,14 @@
 
-//creating websocket for client side / browser cool
-const socket = new WebSocket('ws://localhost:3001')
+//no long using websocket, using socket.io instead
+const socket = new io('ws://localhost:3001')
 
 function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
     //if value was inputeed in html form
     if(input.value){
-        socket.send(input.value) //sending exact same message received to socket
+        //instead of send, we are emitting a message
+        socket.emit('message',input.value) //sending exact same message received to socket
         input.value = "" //resetting message, erasing whats in input
     }
     input.focus()
@@ -17,8 +18,9 @@ function sendMessage(e) {
 document.querySelector('form')
   .addEventListener('submit', sendMessage) //addEventListener isten for specfic action (pressing submit) and does a function when it occurs
 
-//Listening for messages
-socket.addEventListener("message", ({ data }) => {
+//listen for messages
+//No longer using websockets, instead of addEvenetListener, we are using 'on' - when server receives message, on() calls specified function
+socket.on("message", (data) => {
     const li = document.createElement('li') //creating list element that will contain message from server
     li.textContent = data //making list contain that message
     document.querySelector('ul').appendChild(li) //grabbing unorders list from original html code and appedning new message to it
--- real_time_chat/server/index.js
+++ real_time_chat/server/index.js
@@ -1,20 +1,36 @@
-//defining ws (websockets)
-//websocket is a communication protocol that allows simulatenous  
-//two way communication over single TCP connection
-const ws = require('ws')
+//no longer using websockets, using socket.io instead
+//socket.io provides built in feutres like automatic reconnection, fallback to HTTP, long-polling and multiplexing
+//websocket requires more complex server and client architecture
+import { createServer } from "http"
+import { Server } from "socket.io"
 
-//defining server and opening server on port 3000
-const server = new ws.Server({ port: '3001'})
+//creating http server
+const httpServer = createServer()
 
-//using server, when theres a connection established with websockets
-server.on('connection', socket => {
-    socket.on('message', message => { //we are then listening for a messag, we take that message with a fucntion once againe
-        //when sending a message in app, we get a buffer message so in terminal <buffer10/34>
-        //to also display the message on console log , do the following, it will then show "hey"
-        const b = Buffer.from(message)
-        console.log(b.toString())
-        //console.log(message)
-        socket.send(`${message}`) //whatever server receives, server is going to echo message and send it back to whoever sent it
+//using created server
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
+    }
+})
+
+
+//socket.io has a connection that emits every message to everyone connected
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`) //displaying message of which specific user connected - each user gets unique id
+
+    socket.on('message', data => { 
+        console.log(data) //displaying data
+        io.emit('message', `${socket.id.substring(0,5)}: ${data}`) //io.emit will emit this message to everyone that is currently connected
+                                                                    //${socket.id.substring(0,5)} will display who sent the message using the unique user if but only displaying first 5 characters
 
     })
 })
+
+//httpServer.listen() is an interface that is used to start server by accepting new connections - making sure server is listening
+//1st argument - port number
+//2nd parameter - optional callback function
+
+//listening on port 3500 and displaying appropiate message using anonymous funnction
+httpServer.listen(3500, () => console.log('listening on port 3500') )
+
