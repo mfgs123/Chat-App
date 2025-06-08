519098f951fd8a199ae1d8b21f1b47125f46b2c2
mfgs123 <mariafernandagranados12@gmail.com>
2025-06-08 14:05:40 +0100

    updating index.js

--- real_time_chat/server/index.js
+++ real_time_chat/server/index.js
@@ -16,6 +16,9 @@ const __dirname = path.dirname(__filename) //__dirname gives you the parent fold
 //this is used when getting our server to listen to requests - PORT is an argument
 const PORT = process.env.PORT || 3500
 
+//creating constant for admin users
+const ADMIN = "Admin"
+
 //refering to our express server as app
 const app = express()
 
@@ -30,6 +33,14 @@ const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
 
+//setting up state for users  - creating a constant that contains 2 objects - array and function
+const UsersState = {
+    "users": [],
+    //users array initially empty and initialised by setUsers function
+    "setUsers": function(newUsersArray) {
+        this.users = newUsersArray
+    }
+}
 //creating express server (instead of http server this time) and returning it to io variable
 const io = new Server(expressServer, {
     //cors (Cross-Origin Resource sharing) is a system
@@ -43,36 +54,158 @@ const io = new Server(expressServer, {
 })
 
 
-//establishing connection
+//establishing connection and interface that user sees
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`) //displaying message of which specific user connected - each user gets unique id
 
-    //upon connection - message only to user that just connected
-    socket.emit('message', "Welcome to Chat App! :) ") //socket.emit goes directly to user
+    //upon connection - message only to user that just connected - using buildMsg() function
+    //where ADMIN user is sending the appropiate message
+    socket.emit('message', buildMsg(ADMIN,"Welcome to Chat App! :) ")) //socket.emit goes directly to user
 
-    //Upon connection - to all others but not user that just connected
-    //socket.broadcast.emit goes to ALL rest of users
-    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`)
+    //listener as  user enters the rrom
+    socket.on('enterRoom', ({name, room}) => {
 
-    //Listening for message event, users receive messages that other users sent
-    socket.on('message', data => { 
-        console.log(data) //displaying data
-        io.emit('message', `${socket.id.substring(0,5)}: ${data}`) //io.emit will emit this message to everyone that is currently connected
-                                                                    //${socket.id.substring(0,5)} will display who sent the message using the unique user if but only displaying first 5 characters
+        //making user leave previous room before entering new room
+        //using getUser and socket.id (socket id is the id of the current user)
+        //getUser() returns user object that matches that id (contain id,room,name)
+        const prevRoom = getUser(socket.id)?.room //using optional chaining that gets room property of that object withoutworrying aboyt TypeError
+
+        //if previous room found
+        if(prevRoom){
+            socket.leave(prevRoom) //user leaves previous room
+            //emitting a message ONLY to that previous room
+            io.to(prevRoom).emit('message', buildMsg(ADMIN,`${name} has left the room`))
+        }
+
+        //activating user
+        const user = activateUser(socket.id, name, room)
+
+        //cannot update previous room users list until after state update in activate user
+        if(prevRoom) {
+            io.to(prevRoom).emit('userList', {
+                users: getUsersInRoom(prevRoom) //updating prev room users list so that it doesnt contain current user
+            })
+        }
+
+        //adding user to room
+        socket.join(user.room)
+
+        //sending a message to ONLY user that joined the room
+        socket.emit('message', buildMsg(ADMIN, `You have joined ${user.room} chat room`))
+
+        //also sending message to everyone else
+        socket.broadcast.to(user.toom).emit('message', buildMsg(ADMIN,`${user.name} has joined the room`))
+
+        //update user list for the room that user justn joined
+        io.to(user.room).emit('userList', {
+            users: getUsersInRoom(user.room)
+        })
+
+        //updatinf user list for everyone / all other rooms
+        io.emit('roomList', {
+            rooms: getAllActiveRooms()
+        })
 
     })
 
+
     //When user disconnects, this will be sent to all tohers
     socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`)
+        //getting a user anjd calling appropiate function
+        const user = getUser(socket.id)
+        userLeavesApp(socket.id) //filters out user from state
+
+        if(user) {
+
+            //emitting appropiate message to users in that chatroom that user left
+            io.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has left the room`))
+
+            //updating list of active users after user leaves room
+            io.to(user.room).emit('userList',{
+                users: getUsersInRoom(user.room)
+            })
+
+            io.emit('roomList', {
+                rooms:getAllActiveRooms()
+            })
+        }
+
+        //to keep it consistent - displaying message on console
+        console.log(`User ${socket.id} disconnected`)
+        
+    })
+
+    //Listening for message event, users receive messages that other users sent
+    socket.on('message', ({name, text}) => { 
+        const room = getUser(socket.id)?.room //like before, only getting room
+        
+        //when room found, we are emitting message and user that sent message to that room
+        if(room){
+            io.to(room).emit('message', buildMsg(name,text))
+        }
     })
 
     //Listen for activity - displaying message when a user is currently typing
     socket.on('activity', (name) => {
-        socket.broadcast.emit('activity', name)
+        const room = getUser(socket.id)?.room
+
+        //broadcast sends who is typing message to ebveryone else, not to the person actually typing
+        if(room) {
+            socket.broadcast.to(room).emit('activity', name)
+        }
+        
     })
 })
 
+//function that builds message that the server sends out - also includes time message was sent
+function buildMsg(name, text) {
+    return {
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', { //time funcion built into javasciprt - no library needed
+              hour: 'numeric',
+              minute: 'numeric',
+              second: 'numeric'
+        }).format(new Date()) //format gives you 1:05:31pm kind of format
+    }  
+}
 
+//user functions - not necessarily when user enters room - when it gets activated
+function activateUser(id, name, room) {
+    const user = { id, name, room } //const that contain objects with relevant info
+    //calling the setUsers function from userState that creates arrays of users using element passed in
+    UsersState.setUsers ([
+        //we use the exisitng users array, we are filtering, returns an array of all users that dont match the user id passed in
+        ...UsersState.users.filter(user => user.id !== id),
+        user //passing in user to array, doing this way  ensures that there in only one unique instance of that id
+    ])
+    return user
+}
 
+//function for when user leaves the chap app
+function userLeavesApp(id) {
+    UsersState.setUsers (
+      UsersState.users.filter(user => user.id !== id) //returns an array containing all users whose if doesnt match id passed in
+      //unlike activateUser(), were not adding the user to the list ath the ebnd
+    )
+} 
 
+//function that returns user object (contains id, room and name) for that specific id
+function getUser(id) {
+    return UsersState.users.find(user => user.id === id)
+}
+
+//function that gets users inside the room specified (passed as argument) 
+//it looks through the array users and filters it to return an array that meets consitions - that user's room matches the room passed as argument
+function getUsersInRoom(room) {
+    UsersState.users.filter(user => user.room === room)
+}
+
+//function that returns an array of all active rooms with NO duplicates
+function getAllActiveRooms() {
+    //Array.from() created an array from the objects passed as argument
+    //map() returns an array of the rooms of each user - there might be duplicates using map()
+     //to prevent duplicates we are creating a new set from map
+     //so array.from() will be needed to convert set into array
+    return Array.from(new Set (UsersState.users.map(user => user.room)))
+}
--- real_time_chat/server/public/app.js
+++ real_time_chat/server/public/app.js
@@ -3,25 +3,69 @@
 const socket = new io('ws://localhost:3500')
 
 //adding query selectors 
+
+
+//msgInput uses a selector that only select one single selector - the form one for the message - that was the only one before
+//however, now we have 2 addtiional inputs for name id and chat room number
+const msgInput = document.querySelector('#message') //points to message entered by user, msgInput is also used in sendMessage(e) function
+const nameInput = document.querySelector('#name')
+const chatRoom = document.querySelector("#room")
+
 //selects activity class that we set up - a paragraphe element
 const activity = document.querySelector('.activity')
-const msgInput = document.querySelector('input') //points to message entered by user, msgInput is also used in sendMessage(e) function
+
+//adding more query selectors for classes user-list and room-list that we set up as paragraph elements in html form
+const userList = document.querySelector('.user-list')
+const roomList = document.querySelector('.room-list')
+const chatDisplay = document.querySelector('.chat-display')
+
 
 
 function sendMessage(e) {
-    e.preventDefault()
-    //if value was inputeed in html form
-    if(msgInput.value){
-        //instead of send, we are emitting a message
-        socket.emit('message',msgInput.value) //sending exact same message received to socket
+    e.preventDefault() //preventDefault() cancels even if it is cancelable - since we are using it with 'submit' button - it will prevent it from submitting form - do the following code instead
+    //if value was inputeed in html form 
+   
+    //checking that both message, name and chat room inputs have some data in them
+    if(msgInput.value && nameInput.value && chatRoom.value){
+        //we want to emit an object that have more than one value
+        socket.emit('message', {
+            name: nameInput.value, //object keys - name and text can work with or without double quotes
+            text: msgInput.value
+        }) 
         msgInput.value = "" //resetting message, erasing whats in input
     }
     msgInput.focus()
 }
+
+//function thats called when user enters a chatroom - it will emit the user id and 
+function enterRoom(e) {
+    e.preventDefault()
+    //this time checking that name and chatroom fields are not empty
+    if(nameInput.value && chatRoom.value){
+        socket.emit('enterRoom', {
+            name: nameInput.value,
+            room: chatRoom.value
+        })
+
+    }
+
+}
+
 //using above function a couple of times
-//when the user presses 'submit' button in form, it will call sendMessage function
-document.querySelector('form')
+//when the user presses 'submit' button in form with class "form-msg", it will call sendMessage function
+document.querySelector('.form-msg')
   .addEventListener('submit', sendMessage) //addEventListener isten for specfic action (pressing submit) and does a function when it occurs
+
+//adding another event listen, that performs enterRoom() function when user presses jon button
+document.querySelector('.form-join')
+ .addEventListener('submit', enterRoom)
+
+//adding an event listener to msg input - we dont have to do document.querySelector - since msgInput was already selected in this case
+//listening for key press event - anonymous function will execute if event trigerred - letting users know what user is currently typing
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', nameInput.value)
+})
 
 //listen for messages
 //No longer using websockets, instead of addEvenetListener, we are using 'on' - when server receives message, on() calls specified function
@@ -32,11 +76,7 @@ socket.on("message", (data) => {
     document.querySelector('ul').appendChild(li) //grabbing unorders list from original html code and appedning new message to it
 })
 
-//adding an event listener to msg input
-//listening for key press event - anonymous function will execute if event trigerred
-msgInput.addEventListener('keypress', () => {
-    socket.emit('activity', socket.id.substring(0,5))
-})
+
 
 //setting up timer, if user takes too long typing a message, this ight mean they gave up and dont want to send message anymore
 //when timer runs out, the user is typing... message gets removed
